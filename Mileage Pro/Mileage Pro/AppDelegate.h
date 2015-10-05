74da2c5d95120bcb6a2061ec630ede9414736e5d
Kris Kata <kata.krzysztof@gmail.com>
2015-10-05 14:42:36 -0500

    Creaetd new project

---
+++ Mileage Pro/Mileage Pro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mileage Pro
+//
+//  Created by Kris Kata on 10/5/15.
+//  Copyright (c) 2015 katak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mileage Pro/Mileage Pro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mileage Pro
+//
+//  Created by Kris Kata on 10/5/15.
+//  Copyright (c) 2015 katak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
