6b79cb25f54eb778fc9a351d4b7e17c21d20c5b2
Kris Kata <kata.krzysztof@gmail.com>
2015-10-06 21:49:18 -0500

    Add vehicle detail input fields

--- Mileage Pro/Mileage Pro/VehicleDetailViewController.m
+++ Mileage Pro/Mileage Pro/VehicleDetailViewController.m
@@ -9,29 +9,16 @@
 #import "VehicleDetailViewController.h"
 
 @interface VehicleDetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *makeField;
+@property (weak, nonatomic) IBOutlet UITextField *modelField;
+@property (weak, nonatomic) IBOutlet UITextField *colorField;
+@property (weak, nonatomic) IBOutlet UITextField *yearField;
 
 @end
 
 @implementation VehicleDetailViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
