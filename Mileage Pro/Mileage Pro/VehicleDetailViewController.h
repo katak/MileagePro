26a34c893284827450cc8cb6bd608facd072a3b4
Kris Kata <kata.krzysztof@gmail.com>
2015-10-06 20:56:01 -0500

    Add vehicle detail controller and empty xib

---
+++ Mileage Pro/Mileage Pro/VehicleDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  VehicleDetailViewController.h
+//  Mileage Pro
+//
+//  Created by Kris Kata on 10/6/15.
+//  Copyright (c) 2015 katak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VehicleDetailViewController : UIViewController
+
+@end
---
+++ Mileage Pro/Mileage Pro/VehicleDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  VehicleDetailViewController.m
+//  Mileage Pro
+//
+//  Created by Kris Kata on 10/6/15.
+//  Copyright (c) 2015 katak. All rights reserved.
+//
+
+#import "VehicleDetailViewController.h"
+
+@interface VehicleDetailViewController ()
+
+@end
+
+@implementation VehicleDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
